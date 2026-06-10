1339f96e0d174af3d52c841272069ac4d3629843
Otrebor19 <96426304+Otrebor19@users.noreply.github.com>
2026-06-10 00:13:18 -0400

    Configurando la produccion

--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
-	process.env.NEXT_PUBLIC_API_URL || "https://tordoya-api.onrender.com";
+  process.env.NEXT_PUBLIC_API_URL || "https://tordoya.onrender.com";
 
 export const AUTH_TOKEN_KEY = "auth_token";
